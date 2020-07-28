861b3aef1e9c194ba56b3a1cb1ccd809135ad403
unknown <vinay.vatsa.007@gmail.com>
2020-07-29 04:04:48 +0530

    pushing the code next session

---
+++ Crud operation/client/assignment_app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import Table from './components/Table';
+// import logo from './logo.svg';
+import "./App.css";
+// import { string } from 'prop-types';
+// import { Tab } from '@material-ui/core';
+import Assignments from "./containers/Assignments/Assignments";
+
+class App extends Component {
+  render() {
+    return <Assignments />;
+  }
+}
+export default App;
