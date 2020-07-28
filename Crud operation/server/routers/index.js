d013f53651383a11e4211a947abe2f030f993f6b
unknown <vinay.vatsa.007@gmail.com>
2020-07-29 03:47:07 +0530

    pushing the code next session

---
+++ Crud operation/server/routers/index.js
@@ -0,0 +1,15 @@
+// for combining all routers from different tables will be combined here in index.js and providing a single access point for routers
+const express = require('express');
+const server = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const assignment = require('./assignment');
+
+server.use(bodyParser.json({extended:true}));
+server.use(cors()); // server.use is used for accessing middleware functions.
+
+//assignments will be considered as assignment/list
+server.use('/assignments', assignment);
+
+module.exports = server;
