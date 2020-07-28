d839c94b92c56cbd7516534dcde6d48bfb11e37a
unknown <vinay.vatsa.007@gmail.com>
2020-07-29 04:07:26 +0530

    pushing the code next session

---
+++ Crud operation/client/assignment_app/src/components/Form.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function Form(props) {
+  const { open, onClose, onSave } = props;
+  return (
+    <div>
+      <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">{props.title}</DialogTitle>
+        <DialogContent>
+          {props.formContent && (
+            <DialogContentText>{props.formContent}</DialogContentText>
+          )}
+          {props.children}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={onSave} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
