3d9774e0e707e2a94fad0f84543d9af3e41ef522
unknown <vinay.vatsa.007@gmail.com>
2020-08-16 18:12:54 +0530

    using the refs via callbacks intead of direct reference

--- Crud operation/client/assignment_app/src/containers/Dashboards/index.js
+++ Crud operation/client/assignment_app/src/containers/Dashboards/index.js
@@ -6,42 +6,62 @@ import SimpleLineChart from "../../components/charts/SimpleLineChart";
 const Dashboard = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState([]),
-    spanRef = useRef(),
-    inputRef = useRef({});
+    [width, setWidth] = useState(100),
+    spanRef = useRef();
+  let inputRef = useRef();
 
   //console.log("data, setData", data, setData);
   const assignmentService = new AssignmentService();
   // const data = assignmentService.countBySubject();
   let countData = [];
+  /* componentDidMount */
   useEffect(() => {
     countBySubject();
-    console.log("COMPONENT DID MOUNT IN Dashboard\n\n\n");
-    inputRef.current.focus();
+    //console.log("COMPONENT DID MOUNT IN Dashboard\n\n\n");
+    if (inputRef) {
+      inputRef.focus();
+      //setWidth(inputRef.offsetWidth + 20);
+    }
   }, []);
 
-  console.log("element \n\n\n", {
+  /*  console.log("element \n\n\n", {
     ele: inputRef.current,
-  });
+  }); */
   const countBySubject = async () => {
     setIsLoading(true);
     const results = await assignmentService.countBySubject();
-    console.log("data, setData in countBySubject", data, setData);
+    //console.log("data, setData in countBySubject", data, setData);
 
-    console.log(
+    /*  console.log(
       "COMPONENT DID MOUNT IN Dashboard -->countBySubject \n\n\n",
       results
-    );
+    ); */
     setData(results);
     setIsLoading(false);
   };
 
-  console.log("data in Render", data);
+  console.log("inputRef in Render", inputRef);
 
+  let width1 = inputRef.current ? inputRef.current.offsetWidth + 20 : 100;
+  //let width1 = 100;
   return (
     <div>
       <h1>Welcome to My Dashboard</h1>
-      <span ref={spanRef}>This if using ref in html</span>
-      <input ref={inputRef} />
+      <input
+        ref={(inp) => {
+          console.log("inp", { inp });
+          if (inp) {
+            inputRef = inp;
+            // inputRef.offsetWidth = 300;
+          }
+        }}
+        //Or direct method
+        //ref={inputRef}
+      />
+      <div ref={spanRef} style={{ width: width1, background: "#111" }}>
+        This if using ref in html
+      </div>
+
       <SimplePieChart data={data} />
       {/*  <SimpleLineChart data={data} /> */}
     </div>
